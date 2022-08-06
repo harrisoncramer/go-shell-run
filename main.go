cd4373a989570c925f8395a8f93db80f3dda9449
Harrison Cramer <harrisoncramer@gmail.com>
2022-08-06 12:41:51 -0400

    changed to mux for router

--- main.go
+++ main.go
@@ -14,13 +14,11 @@ var port string
 
 func main() {
 
-	// mux := http.NewServeMux()
-	// mux.Handle("/status", handlers.StatusHandler)
-
-	http.HandleFunc("/restart", handlers.RestartHandler)
-	http.HandleFunc("/status", handlers.StatusHandler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/status", handlers.StatusHandler)
+	mux.HandleFunc("/restart", handlers.RestartHandler)
 
 	flag.StringVar(&port, "port", ":3012", "Port of server")
 	logger.Log(fmt.Sprintf("Server is starting on port %s", port))
-	log.Fatal(http.ListenAndServe(port, nil))
+	log.Fatal(http.ListenAndServe(port, mux))
 }
