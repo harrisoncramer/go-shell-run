8ffde64039965629e181da2bdb2bbd63ecf918a1
Harrison Cramer <harrisoncramer@gmail.com>
2022-08-07 10:58:15 -0400

    Moved job to separate goroutine

--- handlers/main.go
+++ handlers/main.go
@@ -1,46 +1,15 @@
 package handlers
 
 import (
-	"bufio"
 	"encoding/json"
-	"fmt"
 	"io"
-	"log"
 	"net/http"
-	"os/exec"
-	"strings"
 )
 
 func StatusHandler(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, `{"alive": true }`)
-}
-
-func runJob(command string, args []string) {
-
-	cmd := exec.Command(command, args...)
-
-	stdout, _ := cmd.StdoutPipe()
-
-	stderr, _ := cmd.StderrPipe()
-	if err := cmd.Start(); err != nil {
-		log.Fatal(err)
-	}
-
-	scanner := bufio.NewScanner(stderr)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-
-	outputScanner := bufio.NewScanner(stdout)
-	outputScanner.Split(bufio.ScanLines)
-	for outputScanner.Scan() {
-		m := outputScanner.Text()
-		fmt.Println(m)
-	}
-
-	cmd.Wait()
 }
 
 type Jobs struct {
@@ -57,9 +26,9 @@ func RestartHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	log.Println(jobs.Jobs)
-	for i := 0; i < len(jobs.Jobs); i++ {
-		values := strings.Split(jobs.Jobs[i], " ")
-		runJob(values[0], values[1:])
-	}
+	go RunJobs(jobs.Jobs)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, `{"running": true }`)
+
 }
---
+++ handlers/utils.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+)
+
+func runJob(command string, args []string) {
+
+	cmd := exec.Command(command, args...)
+
+	stdout, _ := cmd.StdoutPipe()
+
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(stderr)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	outputScanner := bufio.NewScanner(stdout)
+	outputScanner.Split(bufio.ScanLines)
+	for outputScanner.Scan() {
+		m := outputScanner.Text()
+		fmt.Println(m)
+	}
+
+	cmd.Wait()
+}
+
+func RunJobs(jobs []string) {
+	for i := 0; i < len(jobs); i++ {
+		values := strings.Split(jobs[i], " ")
+		runJob(values[0], values[1:])
+	}
+}
